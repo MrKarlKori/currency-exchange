a3fe554c02b52dc6630aac56a4b8fc4b72ab9b64
Denis Kurgan <skaller@tut.by>
2018-04-28 01:30:33 +0300

    The very first commit. Added module, components, routes, some layout and logic.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule }   from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
+import { AppComponent } from './app/app.component';
+import { ConvertComponent } from './convert/convert.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { IndexComponent } from './index/index.component';
+
+import { HttpService } from './http.service';
+
+const appRoutes: Routes = [
+  {path: '', component: IndexComponent},
+  {path: 'convert', component: ConvertComponent},
+  {path: '**', component: NotFoundComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConvertComponent,
+    NotFoundComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
